297ca859feb1bf8b78022908124afde6d9176d4a
Spencer Handley <spencerhandley@gmail.com>
2016-02-24 23:12:20 -0800

    correct typo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Chat from './Chat.js'
+import Chat from './Chat.js';
+
 export default class App extends Component {
   render() {
     return <div>
